b6e5f5a9ff8e4cfd85d54e3efa449709a5b2ff01
Genésio <genesio.s.p.28@gmail.com>
2024-10-25 18:05:26 -0300

    docker install

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -117,21 +117,13 @@ install_typescript() {
 # Function to install Docker
 install_docker() {
   echo "Installing Docker..."
-
-  sudo apt-get update
-  sudo apt-get install ca-certificates curl
-  sudo install -m 0755 -d /etc/apt/keyrings
-  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-  sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-  # Add the repository to Apt sources:
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-  sudo usermod -aG docker $USER
+  if command -v apt > /dev/null; then
+    sudo apt install docker-compose
+  elif command -v snap > /dev/null; then
+    sudo snap install docker
+  else
+    echo "Package manager not supported. Please install docker manually."
+  fi
   echo "Docker installed!"
 }
 
