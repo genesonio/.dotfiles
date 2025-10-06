d53e6a3539c5316027c2785a215cf515b9e26413
Genésio <genesio.s.p.28@gmail.com>
2025-10-05 21:09:11 -0300

    random wallpaper sh

--- .local/bin/random_wallpaper.sh
+++ .local/bin/random_wallpaper.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
+
 WALLPAPER_DIR="$HOME/wallpapers"
+INTERVAL=120  # change every 60 seconds
 
 # Get all monitor names
 MONITORS=$(hyprctl monitors | grep "Monitor" | awk '{print $2}')
 
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
-hyprctl hyprpaper preload "$WALLPAPER"
+while true; do
+    # Pick random wallpaper
+    WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
 
-for MON in $MONITORS; do
-    hyprctl hyprpaper wallpaper "$MON,$WALLPAPER"
+    # Preload image
+    hyprctl hyprpaper preload "$WALLPAPER"
+
+    # Apply to all monitors
+    for MON in $MONITORS; do
+        hyprctl hyprpaper wallpaper "$MON,$WALLPAPER"
+    done
+
+    sleep "$INTERVAL"
 done
