e0d773f292f2037f03a29fae8d4ef596c4b436b6
Genésio <genesio.s.p.28@gmail.com>
2025-10-05 21:54:10 -0300

    fix: wallpaper thing

--- .local/bin/random_wallpaper.sh
+++ .local/bin/random_wallpaper.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 WALLPAPER_DIR="$HOME/wallpapers"
-INTERVAL=120  # change every 60 seconds
+INTERVAL=120
 
 # Get all monitor names
 MONITORS=$(hyprctl monitors | grep "Monitor" | awk '{print $2}')
 
 while true; do
     # Pick random wallpaper
-    WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+    WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -path "*/.git/*" | shuf -n 1)
 
     # Preload image
     hyprctl hyprpaper preload "$WALLPAPER"
